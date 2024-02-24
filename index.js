9d96918c21c5cd1b47e2563765bfc13ae42e3b38
Adesiyan Temitope <adesiyantope2014@gmail.com>
2024-02-24 17:44:59 +0100

    webhook

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.get('/api/search', async (req, res) => {
 
 
 app.post('/api/invoice-created', async (req, res) => {
-  console.log(res.body, "webhook");
+  // console.log(res.body, "webhook");
   const { type, data } = res.body;
 
   if (type === "invoice.created") {
